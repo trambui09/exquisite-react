8ebd9a70d5645b42c6fb5bebfe77b49a185dd1f2
Tram Bui <pbui17@cmc.edu>
2021-01-03 12:44:57 -0600

    wrote the onFormSubmit function and onInputChange func

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,12 +5,57 @@ import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = () => {
   const [playerCount, setPlayerCount] = useState(1)
+  const [fields, setFields] = useState({
+    'adj1': '',
+    'noun1': '',
+    'adv': '',
+    'verb': '',
+    'adj2': '',
+    'noun2': ''
+  });
+
+  const onInputChange = (event) => {
+    const {name, value} = event.target
+
+    const newFields = {
+      ...fields
+    }
+
+    newFields[name] = value
+    setFields(newFields)
+
+  }
+
+  const onFormSubmit = () => {
+   
+    // sends the state up to the game
+    // with a callback func
+
+    console.log(fields)
+
+    // increment the playerCount by 1 
+    setPlayerCount(playerCount + 1)
+
+
+
+    // clear the text fields 
+    setFields({
+      'adj1': '',
+      'noun1': '',
+      'adv': '',
+      'verb': '',
+      'adj2': '',
+      'noun2': ''
+    })
+
+  
+  }
 
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{ playerCount }</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form className="PlayerSubmissionForm__form" onSubmit={onFormSubmit} >
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
@@ -21,27 +66,45 @@ const PlayerSubmissionForm = () => {
           <input
             placeholder="adjective"
             type="text" 
+            name='adj1'
+            value={fields.adj1}
+            onChange={onInputChange}
           />
           <input
             placeholder='noun'
             type='text'
+            name='noun1'
+            value={fields.noun1}
+            onChange={onInputChange}
           />
           <input 
             placeholder='adverb'
             type='text'
+            name='adv'
+            value={fields.adv}
+            onChange={onInputChange}
           />
           <input 
             placeholder='verb'
             type='text'
+            name='verb'
+            value={fields.verb}
+            onChange={onInputChange}
           />
           <p>the</p>
           <input 
             placeholder='adjective'
             type='text'
+            name='adj2'
+            value={fields.adj2}
+            onChange={onInputChange}
           />
           <input 
             placeholder='noun'
             type='text'
+            name='noun2'
+            value={fields.noun2}
+            onChange={onInputChange}
           />
 
         </div>
